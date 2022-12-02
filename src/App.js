c58e7953761496187c2ad441a8ca298725475ce0
Huy <trannhathuy1305@gmail.com>
2022-12-02 15:31:36 +0700

    huy

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Article from './components/Article/Article'
+import Brand from './components/Article/Brand'
+import CTA from './components/Article/CTA'
+import Feature from './components/Article/Feature'
+import Navbar from './components/Article/Navbar'
+
+const App = () => {
+  return (
+    <div>
+        <h1>GPT_AI</h1>
+    </div>
+  )
+}
+
+export default App
